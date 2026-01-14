3dbd3d1d91be81eaad359165e6ad71b9773c287b
jruisot1903-daw <jruisot1903@g.educaand.es>
2026-01-14 09:56:25 +0100

    PrimerasPruebas

---
+++ js/main.js
@@ -0,0 +1,18 @@
+let info = document.getElementById("info");
+
+//leer el archivo desde el servidor al cliente
+document.getElementById("leeTxt").addEventListener("click", () =>{
+    let xhr = new XMLHttpRequest(); // creamos el objeto 
+    xhr.responseType = "text";
+
+    xhr.onreadystatechange = function(){
+        console.log("Estado: "+xhr.readyState+" . Nivel: "+xhr.status); // ver como cambian esos valores
+        if((xhr.readyState == 4) && (xhr.status == 200)){ // condiciones para que se haga 
+            document.getElementById("data").value = xhr.response; // lo mostramos por nuestro "data"
+        }
+    }
+// hacemos la conexión ponendo el metodo que vayamos a utilizar y poniendo la direccion a lo que
+// queremos acceder del del servidor
+    xhr.open("GET","http://www.js.es/server/hello.txt"); 
+    xhr.send();
+});
