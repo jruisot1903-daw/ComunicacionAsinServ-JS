f8af5f232c3255ee946163f6e886f858fdc66b60
jruisot1903-daw <jruisot1903@g.educaand.es>
2026-01-21 10:30:28 +0100

    EnviosDe-JSON

--- js/main.js
+++ js/main.js
@@ -1,25 +1,84 @@
 let info = document.getElementById("info");
 
 //leer el archivo desde el servidor al cliente
-document.getElementById("leeTxt").addEventListener("click", () =>{
-    let xhr = new XMLHttpRequest(); // creamos el objeto 
-    xhr.responseType = "text";
+// document.getElementById("btDatos").addEventListener("click", () =>{
+//     let xhr = new XMLHttpRequest(); // creamos el objeto 
+//     xhr.responseType = "text";
 //xhr.responseType = "blob"; para archivos de audio , videos , imagenes , pdf etc...
-    xhr.onreadystatechange = function(){
+//     xhr.onreadystatechange = function(){
         // console.log("Estado: "+xhr.readyState+" . Nivel: "+xhr.status); // ver como cambian esos valores
         // if((xhr.readyState == 4) && (xhr.status == 200)){ // condiciones para que se haga 
         //     document.getElementById("data").value = xhr.response; // lo mostramos por nuestro "data"
         // }
-        info.innerHTML = xhr.response;
-    }
+
+//         info.innerHTML = xhr.response;
+//     }
 // hacemos la conexión ponendo el metodo que vayamos a utilizar y poniendo la direccion a lo que
 // queremos acceder del del servidor
 
-    // xhr.open("GET","http://www.js.es/server/hello.txt"); 
-    // xhr.send();
+    // Para enviar datos al servidor con POST
+    // xhr.open("POST","http://www.js.es/server/hello.php"); 
+    // xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    // xhr.send("login=Pepe&pass=1234");
 
-    // Para enviar datos al servidor
+     //EJEMPLO CON GET
+//     xhr.open("GET","http://www.js.es/server/hello.php?login=Pepe&pass=1234"); 
+//     xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+//     xhr.send();
+// });
+
+//Leer XML desde el servidor 
+// document.getElementById("btDatos").addEventListener("click", () => {
+//     let xhr = new XMLHttpRequest();
+
+//     // xhr.responseType = "text";
+//     xhr.onreadystatechange = function () {
+
+//        if ((xhr.readyState == 4) && (xhr.status == 200)) {
+//         // Procesamos respuesta del server:
+//             var alumnos = xhr.responseXML;
+//             alumnos = alumnos.getElementsByTagName("Alumnos")[0].getElementsByTagName("alumno");
+//             console.dir("Nombre: " + alumnos[1].getElementsByTagName("name")[0].textContent);
+//             info.innerHTML = "Nombre: " + alumnos[1].getElementsByTagName("name")[0].textContent;
+//        }
+//     }
+
+//     xhr.open("GET","http://www.js.es/server/alumnos.xml");
+//     xhr.send();
+
+    
+
+
+// });
+
+//Enviar JSON al  servidor 
+
+var alumno = new Object();
+
+alumno.name = "Pepe";
+alumno.edad = 19;
+alumno.course = 2;
+alumno.fp = "DAW";
+
+
+console.log(alumno);
+console.log(JSON.stringify(alumno));
+
+document.getElementById("btDatos").addEventListener("click", () => {
+    let xhr = new XMLHttpRequest();
+
+    // xhr.responseType = "text";
+    xhr.onreadystatechange = function () {
+
+       if ((xhr.readyState == 4) && (xhr.status == 200)) {
+        // Procesamos respuesta del server:
+            info = xhr.responseXML;
+           
+       }
+    }
+
     xhr.open("POST","http://www.js.es/server/hello.php"); 
-    xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xhr.send("login=Pepe&pass=1234");
+    xhr.send(JSON.stringify(alumno));
+
+    
 });
--- server/hello.php
+++ server/hello.php
@@ -1,8 +1,16 @@
 <?php
 
- $login = $_POST["login"];
- $pass = $_POST["pass"];
+ $login = $_GET["login"];
+ $pass = $_GET["pass"];
 
 echo "Recibido Login: <b>".$login."</b> en el servidor. El password <b style='color:red'>".$pass." </b>es poco seguro";
+// obtener el objeto de un JSON
+
+$data = file_get_contents('php://input');
+$data = json_decode($data,true); // si le ponemos true es un array asociativo
+
+$name = $data['name'];
+
+echo "Recibido name: ".$name;
 
 ?>
